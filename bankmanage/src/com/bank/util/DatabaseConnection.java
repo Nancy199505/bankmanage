fc26c5df54ddc9991dbeb22ca5a75ffc73cdb6b0
Nancy199505 <nanny199505@gmail.com>
2024-11-10 17:25:02 +0530

    Update DatabaseConnection.java

--- bankmanage/src/com/bank/util/DatabaseConnection.java
+++ bankmanage/src/com/bank/util/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/bank_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";  // replace with actual password
+    private static final String PASSWORD = "password";  
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
